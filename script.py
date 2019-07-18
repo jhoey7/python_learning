907e52472f521e47d576a03d321dfe9dd34965e8
joni.iskandar <jhoey.potter7@gmail.com>
2019-07-18 09:53:51 +0700

    Removed Code

--- script.py
+++ script.py
@@ -1,17 +1,4 @@
-import math
-import sys
-from os import rename
-
 import requests
-
-print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
-
-
 r = requests.get("https://www.fontsquirrel.com/fonts/source-code-pro")
 print(r.status_code)
+print(r.ok)
